1904147a947b7a3ecf48612055218f0d0ee7e5b3
Clara <athaydecla@gmail.com>
2026-05-18 11:37:41 -0300

    apiiii

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using VestCareAPI.Data;
-
+using VestCareApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
--- obj/Debug/net10.0/VestCareAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/VestCareAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VestCareAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab7224da1060730f5334a4c8127243ca522f11f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2154cfc629c487170dc4c089f07c3c47adfe50e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VestCareAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VestCareAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
